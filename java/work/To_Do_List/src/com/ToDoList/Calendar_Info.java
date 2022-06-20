56f757013baea326fc6f1776570b4066128b0a55
ProSleeper <ingngnin@gmail.com>
2022-06-21 00:14:49 +0900

    윤년 오류부분 수정, 다음은 버튼 눌러서 이동하는 부분을 메서드로 빼면 될듯

--- java/work/To_Do_List/src/com/ToDoList/Calendar_Info.java
+++ java/work/To_Do_List/src/com/ToDoList/Calendar_Info.java
@@ -2,51 +2,27 @@ package com.ToDoList;
 
 
 public class Calendar_Info {
-
-	//날짜를 구하는 공식 (일명 만년 달력)
-	/*
-	1. 기본적으로 오늘이 언제인지 알아야한다.(컴퓨터 내부적으로 혹은 웹에서 오늘이 언제인지, 기준일로부터 몇일째인지 등)
-	2. 윤년을 구하는 공식을 사용해서 정확한 공식을 도출
-	3. 年은 기준을 윤년공식으로 나누면 현재 년도가 나온다.(이때 올해는 아직 다 지나가지 않았기 때문에 2021년도까지만 구함
-	4. 月은 올해를 기준으로 2022년도 1월1일을 기준으로 나누면 된다.
-	5.週는 나눈 월을 가지고 7로 나누어서 구하면 된다.
-	6. 日은 월을 기준으로 오늘이 며칠째인지 구하면 된다.
-	7. 모든 일이 데이터(todolist)를 가지고 있지는 않으니 해당일을 클릭해서 todolist를 만들면 동적으로 생성되게 하자.
-	8. 날짜버튼을 누르면 todolist가 출력된 showtodo패널에서 다 지우고 다시 그 날짜의 todolist를 출력해주면 된다.
-	*/
-
 	private static Calendar_Info ci = null;
 	private static int[] MonthCount = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};	//이건 윤년 것도 필요할지 고민
 
-
-	//아래 값들은 set메서드로 설정하면 다 바뀔 수 있도록 구현
+	//아래 cur변수들은 맨 처음 초기화하고 오늘을 알 수 있는 용도로 쓰자
 	static private int 		curYear = 0;		//	연 (기본값은 올해)
 	static private boolean 	curIsLeaf = false;	//	연도가 윤년인지
-
 	static private int 		curMonth = 0;		//	월 (기본값은 이번 달)
 	static private int 		curDayOfWeek = 0;	//	현재 적용된 월의 1일의 요일
-
 	static private int 		curDay = 0;			//	날 (기본값은 오늘)
 	
 	
-	
-	
-	
-	
+	//아래 set변수들은 계속해서 변경되는 날짜를 위해서 쓰자.
 	static private int 		setYear = 0;		//	연 (기본값은 올해)
 	static private boolean 	setIsLeaf = false;	//	연도가 윤년인지
-
 	static private int 		setMonth = 0;		//	월 (기본값은 이번 달)
 	static private int 		setDayOfWeek = 0;	//	현재 적용된 월의 1일의 요일
-
 	static private int 		setDay = 0;			//	날 (기본값은 오늘)
-	
-	
-	
 
 	private Calendar_Info(){
 		initializeCalendar();
-		setCalendar(2018, 5);
+		//setCalendar(2018, 5);
 	}
 
 	public static Calendar_Info getInstance() {
@@ -69,7 +45,7 @@ public class Calendar_Info {
 		int presentDay = 1970;//현재 System.currentTimeMillis();로 시간 구한다. 1970년1월1일부터 현재까지의 밀리초를 반환해준다. 그래서 1970
 		int countOfYear = 0;	//윤년이면 366 평년이면 365을 저장할 변수
 		int yearOfMonth = 0;
-		boolean isLeaf = false;
+		boolean isLeaf = true;
 		
 		
 		miliseconds = System.currentTimeMillis();
@@ -88,14 +64,18 @@ public class Calendar_Info {
 			nDay -= countOfYear;
 			presentDay++;
 		}
+		
+		if ((presentDay % 4) != 0 || (presentDay % 100) == 0 && (presentDay % 400) != 0) {
+			isLeaf = false;
+		}
 
 		for (yearOfMonth = 0; yearOfMonth < MonthCount.length; yearOfMonth++) {
 
-			if (nDay <= MonthCount[yearOfMonth]) {
+			if (nDay <= MonthCount[yearOfMonth] && !isLeaf || nDay <= 29 && isLeaf) {
 				break;
 			}
-
-			if((countOfYear % 365) == 1 && yearOfMonth == 1) {
+			
+			if(yearOfMonth == 1 && isLeaf) {
 				//윤년
 				nDay -= 29;
 				continue;
@@ -105,26 +85,37 @@ public class Calendar_Info {
 
 		dayOfTheWeek = (dayOfTheWeek - nDay + 4) % 7;	//이 값은 현재 이번달의 시작 요일이 저장됨.
 		
-		if ((presentDay % 4) != 0 || (presentDay % 100) == 0 && (presentDay % 400) != 0) {
-			isLeaf = false;
-		}
-		else {
-			isLeaf = true;
-		}	 
+		curYear = presentDay;		    //	연 (기본값은 올해)
+		curIsLeaf = isLeaf;			    //	연도가 윤년인지
+		curMonth = yearOfMonth + 1;	    //	월 (기본값은 이번 달)
+		curDayOfWeek = dayOfTheWeek;    //	현재 적용된 월의 1일의 요일
+		curDay = nDay;				    //	날 (기본값은 오늘)
 		
-		curYear = presentDay;		//	연 (기본값은 올해)
-		curIsLeaf = isLeaf;			//	연도가 윤년인지
-		curMonth = yearOfMonth + 1;	//	월 (기본값은 이번 달)
-		curDayOfWeek = dayOfTheWeek;//	현재 적용된 월의 1일의 요일
-		curDay = nDay;				//	날 (기본값은 오늘)
-
+		
+		//바뀔 값
+		setYear = curYear;				//	연 (기본값은 올해)
+		setIsLeaf = curIsLeaf;			//	연도가 윤년인지
+		setMonth = curMonth;			//	월 (기본값은 이번 달)
+		setDayOfWeek = curDayOfWeek;	//	현재 적용된 월의 1일의 요일
+		setDay = curDay;				//	날 (기본값은 오늘)
 	}
 
 
 	//버튼을 누르면 1달씩 바꿔서 출력해줄 부분
 	public static void setCalendar(int pYear, int pMonth) {
 		
-//		int nSetDay = (pYear - 1) * 365 + (pYear - 1) / 4 - (pYear - 1) / 100 + (pYear - 1) / 400;
+		if (pMonth < 1) {
+			pMonth = 1;
+		}
+		
+		if (pMonth > 12) {
+			pMonth = 12;
+		}
+		
+		if (pYear < 1970) {
+			pYear = 1970;
+		}
+		
 		int nSetDay = (pYear - 1) * 365 + (pYear - 1) / 4 - (pYear - 1) / 100 + (pYear - 1) / 400;
 		int nSetNine = (1969) * 365 + (1969) / 4 - (1969) / 100 + (1969) / 400;
 		int yearOfDay = 0;	
@@ -134,8 +125,6 @@ public class Calendar_Info {
 		if ((pYear % 4) != 0 || (pYear % 100) == 0 && (pYear % 400) != 0) {
 			leafYear = false;
 		}
-		
-		
 		nSetDay -= nSetNine;
 		
 		for (int i = 0; i < pMonth - 1; i++) {
@@ -162,10 +151,9 @@ public class Calendar_Info {
 
 	public static String monthOfMaxDay(int pMonth, int pDay)
 	{
-		if(setIsLeaf && pMonth == 2 && pDay == 29) {
+		if((curIsLeaf || setIsLeaf) && pMonth == 2 && pDay == 29) {
 			return "29";
 		}
-		
 		
 		if (MonthCount[pMonth - 1] < pDay) {
 			return "";
@@ -252,5 +240,4 @@ public class Calendar_Info {
 	public static void setSetDay(int setDay) {
 		Calendar_Info.setDay = setDay;
 	}
-	
 }
--- java/work/To_Do_List/src/com/ToDoList/Calendar_Panel.java
+++ java/work/To_Do_List/src/com/ToDoList/Calendar_Panel.java
@@ -6,6 +6,7 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import javax.swing.BorderFactory;
@@ -20,8 +21,11 @@ public class Calendar_Panel extends JPanel {
 	String[] week = {"일", "월", "화", "수", "목", "금", "토"};
 	String[] arrow = {"<<", ">>"};
 
-	JButton bts;
-
+	ArrayList<JButton> buttonArr = new ArrayList<>();
+	JButton yearTitle = null;
+	JButton monthTitle = null;
+	JButton leftArrow = null;
+	JButton rightArrow = null;
 
 	public Calendar_Panel() {
 		this.setBackground(Color.orange);
@@ -33,27 +37,23 @@ public class Calendar_Panel extends JPanel {
 	public void buttonClick(){
 	}
 
-
 	//캘린더 출력 초기화(키면 항상 오늘이 나오도록)
 	public void initCalendar() {
 		Calendar_Info.initializeCalendar();
 
 		//월 이동 버튼 생성
-		bts = addButton(arrow[0]);
+		leftArrow = addButton(arrow[0]);
 		this.add(new JLabel());
 		this.add(new JLabel());
-		addButton(String.valueOf(Calendar_Info.getCurMonth()));
+		monthTitle = addButton(String.valueOf(Calendar_Info.getCurMonth()));
 		this.add(new JLabel());
 		this.add(new JLabel());
-		addButton(arrow[1]);
+		rightArrow = addButton(arrow[1]);
 		//무식해도 차라리 이게 더 직관적이고 수정하기 편할듯
 
 		for (int i = 0; i < GRID_SIZE; i++) {
 			addButton(week[i]);
 		}
-
-
-
 
 		String viewDay = "";
 		int dayOfWeek = Calendar_Info.getCurDayOfWeek();
@@ -61,59 +61,34 @@ public class Calendar_Panel extends JPanel {
 
 		for (int i = 1; i < GRID_SIZE; i++) {
 			for (int j = 0; j < GRID_SIZE; j++) {
-
 				if(dayOfWeek-- <= 0) {
 					viewDay = Calendar_Info.monthOfMaxDay(Calendar_Info.getCurMonth(), countDay++);
 				}
-
-				addButton(viewDay);
+				buttonArr.add(addButton(viewDay));
 			}
 		}
 	}
 
-
 	public void setCalendar() {
-		System.out.println("실행");
-		this.removeAll();
-		Calendar_Info.setCalendar(2022, 5);
-
-		//월 이동 버튼 생성
-		addButton(arrow[0]);
-		this.add(new JLabel());
-		this.add(new JLabel());
-		addButton(String.valueOf(Calendar_Info.getSetMonth()));
-		this.add(new JLabel());
-		this.add(new JLabel());
-		addButton(arrow[1]);
-		//무식해도 차라리 이게 더 직관적이고 수정하기 편할듯
-
-		for (int i = 0; i < GRID_SIZE; i++) {
-			addButton(week[i]);
-		}
+		Calendar_Info.setCalendar(1970, 1);
+		
+		monthTitle.setText(""+(Calendar_Info.getSetMonth()));
 
 		String viewDay = "";
 		int dayOfWeek = Calendar_Info.getSetDayOfWeek();
 		int countDay = 1;
-
-		for (int i = 1; i < GRID_SIZE; i++) {
-			for (int j = 0; j < GRID_SIZE; j++) {
-
-				if(dayOfWeek-- <= 0) {
-					viewDay = Calendar_Info.monthOfMaxDay(Calendar_Info.getSetMonth(), countDay++);
-				}
-
-				addButton(viewDay);
+		
+		for (int i = 0; i < buttonArr.size(); i++) {
+			buttonArr.get(i).setText("");
+			if(dayOfWeek-- <= 0) {
+				viewDay = Calendar_Info.monthOfMaxDay(Calendar_Info.getSetMonth(), countDay++);
+				buttonArr.get(i).setText(viewDay);
 			}
 		}
-
-		this.revalidate();
-		this.repaint();
-
-
 	}
 
 	public void btnClick() {
-		bts.addMouseListener(new MouseAdapter() { //클래스 이름없이 어뎁터 클래스 생성
+		rightArrow.addMouseListener(new MouseAdapter() { //클래스 이름없이 어뎁터 클래스 생성
 			public void mousePressed(MouseEvent e) {
 				if(e.getButton() == MouseEvent.BUTTON1) {
 					setCalendar();
