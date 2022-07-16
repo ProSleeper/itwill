e2d4c5e848de48461865301fd0672b3a8dcb478a
ProSleeper <ingngnin@gmail.com>
2022-07-17 00:46:07 +0900

    naver 로그인 아이디부터 이름까지 제한조건 구현 완료.
    나머지 제한조건은 내일 구현하고 db까지 붙이면 끝

--- Study/WebContent/work/SignUp.js
+++ Study/WebContent/work/SignUp.js
@@ -30,65 +30,94 @@ class CellPhone {
 }
 
 class PassWord {
-    check(params) {
-        // // alert(params);
-        // infoText = document.getElementById("alert_text_PW");
-        // infoText.style = "display: default; color: red";
-        // // var inputText = String(document.getElementById("inputbox_ID").value);
-        // const regex = /^[0-9a-z]{1}[0-9a-z_-]{4,19}$/;
-        // if (params.value.length == 0) {
-        //     infoText.innerText = "필수 정보입니다.";
-        // } else if (/*db적용시 중복값 체크*/ null) {
-        //     alert("중복값 체크");
-        // } else if (!regex.test(params.value)) {
-        //     infoText.innerText = "5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.";
-        // } else {
-        //     infoText.style = "display: default; color: green";
-        //     infoText.innerText = "멋진 아이디네요!";
-        // }
+    constructor() {
+        this.originPW = null;
+    }
+    check(pInputBoxText) {
+        this.originPW = pInputBoxText.value;
+        var infoText = document.getElementById("alert_text_PW");
+        infoText.style = "display: default; color: red";
+
+        //실제로는 조건이 아주 복잡한 것 같다.
+        //문서로 정리되어있으면 몰라도 그걸 일일히 찾는 건 시간낭비 같다.
+        //그냥 아래 조건정도만 체크하자
+        const regex = /^[\w\~\`\!\@\#\$\%\^\&\*\(\)\=\+\|\.\<\,\>\/\?\"\'\:\;\[\]\{\}]{8,16}$/;
+        if (this.originPW.length == 0) {
+            infoText.innerText = "필수 정보입니다.";
+        } else if (!regex.test(this.originPW)) {
+            infoText.innerText = "8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.";
+        } else {
+            infoText.innerText = "";
+            // 여기서 자물쇠 부분을 바꿔주면 될듯
+            // 자세한 판단방법은 나도 모르니 그냥 내가 알아서~
+        }
+
+        /*
+    CAPS LOCK 켜져있으면 켜져 있다고 알려줌
+    알려주는 조건은 문자 입력시에만인듯. 숫자 입력하니까 메세지 사라짐
+    그러다가 다시 문자 입력하면 알려줌
+    1A1A입력하면 나왔다가 사라졌다가 반복함
+    Caps Lock 이 켜져 있습니다.
+
+    *아이콘* 
+    1.기본: 회색 점표시 자물쇠(문자는 없음)
+    2.마우스 클릭 후 focus를 잃었을 때
+    -필수 정보입니다.
+    2.사용불가: 빨간 점표시 자물쇠(자물쇠 왼쪽 "사용불가" 적힘)
+    -8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.
+    3.보통: 연한 주황색 자물쇠(자물쇠 왼쪽 "보통" 적힘)(이 조건은 비밀번호로 사용은 가능한듯)
+    4.위험: 진한 주황색 자물쇠(자물쇠 왼쪽 "위험" 적힘)
+    -8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.
+    5.안전: 녹색 자물쇠(자물쇠 왼쪽 "안전" 적힘)
+    */
     }
 
-    /*
-	CAPS LOCK 켜져있으면 켜져 있다고 알려줌
-	알려주는 조건은 문자 입력시에만인듯. 숫자 입력하니까 메세지 사라짐
-	그러다가 다시 문자 입력하면 알려줌
-	1A1A입력하면 나왔다가 사라졌다가 반복함
-	Caps Lock 이 켜져 있습니다.
+    reconfirm(pInputBoxText) {
+        var reconfirmPW = pInputBoxText.value;
+        var infoText = document.getElementById("alert_text_CPW");
+        infoText.style = "display: default; color: red";
 
-	*아이콘* 
-	1.기본: 회색 점표시 자물쇠(문자는 없음)
-	2.마우스 클릭 후 focus를 잃었을 때
-	-필수 정보입니다.
-	2.사용불가: 빨간 점표시 자물쇠(자물쇠 왼쪽 "사용불가" 적힘)
-	-8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.
-	3.보통: 연한 주황색 자물쇠(자물쇠 왼쪽 "보통" 적힘)(이 조건은 비밀번호로 사용은 가능한듯)
-	4.위험: 진한 주황색 자물쇠(자물쇠 왼쪽 "위험" 적힘)
-	-8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.
-	5.안전: 녹색 자물쇠(자물쇠 왼쪽 "안전" 적힘)
-	*/
-
-    reconfirm() {
-        alert("비밀번호 재확인");
+        //실제로는 조건이 아주 복잡한 것 같다.
+        //문서로 정리되어있으면 몰라도 그걸 일일히 찾는 건 시간낭비 같다.
+        //그냥 아래 조건정도만 체크하자
+        if (reconfirmPW.length == 0) {
+            infoText.innerText = "필수 정보입니다.";
+        } else if (this.originPW != reconfirmPW) {
+            infoText.innerText = "비밀번호가 일치하지 않습니다.";
+        } else {
+            infoText.innerText = "";
+            // 여기서 자물쇠 부분을 바꿔주면 될듯
+            // 자세한 판단방법은 나도 모르니 그냥 내가 알아서~
+        }
     }
 
-    isCapsLockOn(params) {
-        // 여기가 에러
-        if (Event.getModifierState("CapsLock")) {
-            console.log("캡");
+    isCapsLockOn(event, alert_text) {
+        if (event.key == "CapsLock") {    //네이버와 동일한 구현을 위한 코드
+            return;
+        }
+        var infoText = document.getElementById(alert_text);
+        infoText.style = "display: default; color: red";
+
+        if (event.getModifierState("CapsLock")) {
+            infoText.innerText = "Caps Lock 이 켜져 있습니다.";
+        }
+        else {
+            infoText.innerText = "";
         }
     }
 }
+
 let chkPW = new PassWord();
 
 // 아래 테스트 방식으로 경고 만들면 될듯.
 function test() {
-    document.getElementById("alert_text_ID").style = "display: default; color: red";
+    document.getElementById("alert_text_ID").style = "display: default; color: rd";
 }
 
 function test2() {
     document.getElementById("alert_text_ID").style = "display: none;";
 }
-function checkID(params) {
+function checkID(pInputBoxText) {
     // alert(params);
     infoText = document.getElementById("alert_text_ID");
 
@@ -96,11 +125,11 @@ function checkID(params) {
     // var inputText = String(document.getElementById("inputbox_ID").value);
     const regex = /^[0-9a-z]{1}[0-9a-z_-]{4,19}$/;
 
-    if (params.value.length == 0) {
+    if (pInputBoxText.value.length == 0) {
         infoText.innerText = "필수 정보입니다.";
     } else if (/*db적용시 중복값 체크*/ null) {
         alert("중복값 체크");
-    } else if (!regex.test(params.value)) {
+    } else if (!regex.test(pInputBoxText.value)) {
         infoText.innerText = "5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.";
     } else {
         infoText.style = "display: default; color: green";
@@ -118,13 +147,27 @@ function checkID(params) {
     //  -필수 정보입니다.
 }
 
-function setFocus(params) {
-    document.getElementById(params).focus();
+function checkName(pInputBoxText) {
+    var infoText = document.getElementById("alert_text_Name");
+    infoText.style = "display: default; color: red";
+
+    //실제로는 조건이 아주 복잡한 것 같다.
+    //문서로 정리되어있으면 몰라도 그걸 일일히 찾는 건 시간낭비 같다.
+    //그냥 아래 조건정도만 체크하자
+    const regex = /^[0-9a-zA-Z가-힣]{1,40}$/;
+    if (pInputBoxText.value.length == 0) {
+        infoText.innerText = "필수 정보입니다.";
+    } else if (!regex.test(pInputBoxText.value)) {
+        infoText.innerText = "한글과 영문 대 소문자를 사용하세요. (특수기호, 공백 사용 불가)";
+    } else {
+        infoText.innerText = "";
+    }
 }
 
-function checkName() {
-    alert("이름 확인");
-}
+
+// <!-- 40칸 -->
+//                 <!-- 이름에 숫자가 잔뜩들어가도 딱히 뭐라 메세지가 없는 걸 보면 -->
+//                 <!-- 한글과 영문 대 소문자를 사용하세요. (특수기호, 공백 사용 불가) -->
 
 function checkGender() {
     alert("성별 확인");
@@ -136,4 +179,9 @@ function checkEmail() {
 
 function signUpClick() {
     alert("가입하기 버튼");
+}
+
+//각 텍스트 클릭시 해당 inputbox로 focus 해줌.
+function setFocus(params) {
+    document.getElementById(params).focus();
 }
